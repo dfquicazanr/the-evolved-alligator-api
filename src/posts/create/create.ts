00f542af0e13d25a421cfabea3061622e3162627
Daniel Quicazan <dfquicazanr@unal.edu.co>
2021-06-14 17:17:50 -0500

    create post workflow changed

--- src/posts/create/create.ts
+++ src/posts/create/create.ts
@@ -3,9 +3,16 @@ import {sanitize} from "~utils/s3-key-sanitizer";
 import {S3Service} from "~utils/s3-service";
 import {DynamoService} from "~utils/dynamo-service";
 import {itemKey} from "~utils/keys-utils";
+import {dateToPath} from "~utils/date-utils";
 
 export const handler = async (event) => {
   const attributes = HttpEvent.extractBody(event);
+  let { resources } = attributes;
+  const datePath = dateToPath(new Date());
+  const postKey = itemKey('post')
+  resources = resources.map(resource => ({...resource, s3Path: `${datePath}/${postKey}/${sanitize(resource.filename)}`}));
+  resources = await resources.map(async resource => ({...resource, s3SignedUrl: await s3Service.getSignedS3Url('tea-posts', resource.s3Path)}));
+  console.log(resources);
   const s3Service = new S3Service();
   const { fileUrl } = attributes;
   attributes.fileUrl = `https://${sanitize(fileUrl.split('https://')[1])}`;
