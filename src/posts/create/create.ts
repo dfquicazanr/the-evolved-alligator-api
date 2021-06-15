21d74b5b1eb54a2a7ae90fe69e259bb748a51638
Daniel Quicazan <dfquicazanr@unal.edu.co>
2021-06-14 22:11:57 -0500

    modified pos

--- src/posts/create/create.ts
+++ src/posts/create/create.ts
@@ -7,17 +7,15 @@ import {dateToPath} from "~utils/date-utils";
 
 export const handler = async (event) => {
   const attributes = HttpEvent.extractBody(event);
+  console.log(attributes);
   let { resources } = attributes;
   const datePath = dateToPath(new Date());
   const postKey = itemKey('post')
   resources = resources.map(resource => ({...resource, s3Path: `${datePath}/${postKey}/${sanitize(resource.filename)}`}));
+  console.log(resources);
   resources = await resources.map(async resource => ({...resource, s3SignedUrl: await s3Service.getSignedS3Url('tea-posts', resource.s3Path)}));
   console.log(resources);
   const s3Service = new S3Service();
-  const { fileUrl } = attributes;
-  attributes.fileUrl = `https://${sanitize(fileUrl.split('https://')[1])}`;
-  const file = await s3Service.getItem('tea-posts', fileUrl.split('https://tea-posts.s3.amazonaws.com/'));
-  console.log(file);
   const dynamoClient = new DynamoService();
   const result = await dynamoClient.putItem('tea-table', 'post', itemKey('post'), attributes);
   console.log(result);
